3ae3c119ead8c42380e7e8a1da8bec92a561f2da
LM Train <layconmm@msn.com>
2020-01-17 21:58:12 -0600

    Implement Dynamic  Polymorphism Part 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP
 {
@@ -6,11 +7,31 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            WebDeveloper mike = new WebDeveloper("Angular6");
-            mike.Code();
-            mike.Code("C#");
-            mike.Code("C#", "HTML");
-            mike.Code(null, "HTML");
+          List<Object> salesmen = new List<object>() {new CarSalesman("James", "Weaver"), new CarSalesman("Sue", "McGregor"), new RetailSalesPerson("Delores", "Kirby"), new InsuranceBroker("Laycon", "M")};
+
+          foreach(var item in salesmen)
+          {
+              ShowMeHowToSell(item);
+          }
+        }
+
+        static void ShowMeHowToSell(Object salesmen)
+        {
+            if (salesmen is CarSalesman)
+            {
+                CarSalesman carSalesman = (CarSalesman) salesmen;
+                carSalesman.Sell();
+            }
+            else if (salesmen is RetailSalesPerson)
+            {
+                RetailSalesPerson retailSalesPerson = (RetailSalesPerson) salesmen;
+                retailSalesPerson.Sell();
+            }
+            else
+            {
+                InsuranceBroker insuranceBroker = (InsuranceBroker) salesmen;
+                insuranceBroker.Sell();
+            }
         }
     }
 }
