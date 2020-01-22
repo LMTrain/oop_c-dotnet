536a9a3df542f7b1d177dfa31497714f9e5eceab
LM Train <layconmm@msn.com>
2020-01-22 15:37:52 -0600

    Code Example on Boxing and Unboxing

---
+++ C_NetCore2/Day02/Day02/Ex10_Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+
+namespace Day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSharpTypes.Display();
+            ReadLine();
+        }
+    }
+}
--- C_NetCore2/Day02/Day02/Program.cs
+++ C_NetCore2/Day02/Day02/Program.cs
@@ -8,8 +8,19 @@ namespace Day02
     {
         static void Main(string[] args)
         {
-            CSharpTypes.Display();
-            ReadLine();
+            int thisIsvalueTypeVariable = 786;
+            object thisIsObjectTypeVariable = thisIsvalueTypeVariable; //Boxing
+            thisIsvalueTypeVariable += 1;
+            WriteLine("Boxing");
+            WriteLine($"Before boxing: Value of {nameof(thisIsvalueTypeVariable)}: {thisIsvalueTypeVariable}");
+            WriteLine($"After boxing: Value of {nameof(thisIsObjectTypeVariable)}: {thisIsObjectTypeVariable}");
+
+            thisIsObjectTypeVariable = 1900;
+            thisIsvalueTypeVariable = (int)thisIsObjectTypeVariable; //Unboxing
+            WriteLine("Unboxing");
+            WriteLine($"Before Unboxing: Value of {nameof(thisIsObjectTypeVariable)}: {thisIsObjectTypeVariable}");
+            WriteLine($"After Unboxing: Value of {nameof(thisIsvalueTypeVariable)}: {thisIsvalueTypeVariable}");
+
         }
     }
 }
