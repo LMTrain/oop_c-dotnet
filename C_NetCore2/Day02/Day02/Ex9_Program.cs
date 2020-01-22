28fd46dc0e7114ecad843435710ff3f87742bf41
LM Train <layconmm@msn.com>
2020-01-22 15:35:06 -0600

    Code Example on CSharp keyword types

---
+++ C_NetCore2/Day02/Day02/Ex9_Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+
+namespace Day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic dynamicInt = 10;
+            dynamic dynamicString = "This is a string";
+            object obj = 10;
+            WriteLine($"Run-time type of {nameof(dynamicInt)} is {dynamicInt.GetType()}");
+            WriteLine($"Run-time type of {nameof(dynamicString)} is {dynamicString.GetType()}");
+            WriteLine($"Run-time type of {nameof(obj)} is {obj.GetType()}");
+            ReadLine();
+
+        }
+    }
+}
--- C_NetCore2/Day02/Day02/Program.cs
+++ C_NetCore2/Day02/Day02/Program.cs
@@ -8,14 +8,8 @@ namespace Day02
     {
         static void Main(string[] args)
         {
-            dynamic dynamicInt = 10;
-            dynamic dynamicString = "This is a string";
-            object obj = 10;
-            WriteLine($"Run-time type of {nameof(dynamicInt)} is {dynamicInt.GetType()}");
-            WriteLine($"Run-time type of {nameof(dynamicString)} is {dynamicString.GetType()}");
-            WriteLine($"Run-time type of {nameof(obj)} is {obj.GetType()}");
+            CSharpTypes.Display();
             ReadLine();
-
         }
     }
 }
