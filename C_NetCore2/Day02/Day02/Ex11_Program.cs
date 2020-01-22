07b6ca38471363053dd9d6854ef450f2f7e0c4fa
LM Train <layconmm@msn.com>
2020-01-22 16:21:20 -0600

    Code Example on All CSharp Operators

---
+++ C_NetCore2/Day02/Day02/Ex11_Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+
+namespace Day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int thisIsvalueTypeVariable = 786;
+            object thisIsObjectTypeVariable = thisIsvalueTypeVariable; //Boxing
+            thisIsvalueTypeVariable += 1;
+            WriteLine("Boxing");
+            WriteLine($"Before boxing: Value of {nameof(thisIsvalueTypeVariable)}: {thisIsvalueTypeVariable}");
+            WriteLine($"After boxing: Value of {nameof(thisIsObjectTypeVariable)}: {thisIsObjectTypeVariable}");
+
+            thisIsObjectTypeVariable = 1900;
+            thisIsvalueTypeVariable = (int)thisIsObjectTypeVariable; //Unboxing
+            WriteLine("Unboxing");
+            WriteLine($"Before Unboxing: Value of {nameof(thisIsObjectTypeVariable)}: {thisIsObjectTypeVariable}");
+            WriteLine($"After Unboxing: Value of {nameof(thisIsvalueTypeVariable)}: {thisIsvalueTypeVariable}");
+
+        }
+    }
+}
--- C_NetCore2/Day02/Day02/Program.cs
+++ C_NetCore2/Day02/Day02/Program.cs
@@ -8,19 +8,15 @@ namespace Day02
     {
         static void Main(string[] args)
         {
-            int thisIsvalueTypeVariable = 786;
-            object thisIsObjectTypeVariable = thisIsvalueTypeVariable; //Boxing
-            thisIsvalueTypeVariable += 1;
-            WriteLine("Boxing");
-            WriteLine($"Before boxing: Value of {nameof(thisIsvalueTypeVariable)}: {thisIsvalueTypeVariable}");
-            WriteLine($"After boxing: Value of {nameof(thisIsObjectTypeVariable)}: {thisIsObjectTypeVariable}");
-
-            thisIsObjectTypeVariable = 1900;
-            thisIsvalueTypeVariable = (int)thisIsObjectTypeVariable; //Unboxing
-            WriteLine("Unboxing");
-            WriteLine($"Before Unboxing: Value of {nameof(thisIsObjectTypeVariable)}: {thisIsObjectTypeVariable}");
-            WriteLine($"After Unboxing: Value of {nameof(thisIsvalueTypeVariable)}: {thisIsvalueTypeVariable}");
-
+            WriteLine("Operators example\n");
+            var csharpOperator = new CSharpOperators();
+            Write("Enter first number:");
+            csharpOperator.Num1 = Convert.ToInt32(ReadLine());
+            Write("Enter second number:");
+            csharpOperator.Num2 = Convert.ToInt32(ReadLine());
+            Clear();
+            csharpOperator.Display();
+            ReadLine();
         }
     }
 }
