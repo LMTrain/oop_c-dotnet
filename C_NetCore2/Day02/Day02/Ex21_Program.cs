cf443a12a9085278369dea86b6ef8be515d94a5a
LM Train <layconmm@msn.com>
2020-01-23 14:24:20 -0600

    Code example Jagged array

---
+++ C_NetCore2/Day02/Day02/Ex21_Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+using static System.Convert;
+
+
+namespace Day02
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            WriteLine("Two-Dimentional array. \n");
+
+            int[,] numbers = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+            for (int rowsIndex = 0; rowsIndex < 2; rowsIndex++)
+            {
+                for (int colIndex = 0; colIndex < 2; colIndex++)
+                {
+                    WriteLine($"numbers[{rowsIndex},{colIndex}] = {numbers[rowsIndex, colIndex]}");
+                }
+            }
+            ReadLine();
+        }
+       
+    }
+}
