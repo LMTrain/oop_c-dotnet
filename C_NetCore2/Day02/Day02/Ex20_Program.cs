08de06738421912c7c4563a0e17c62883b5cb71e
LM Train <layconmm@msn.com>
2020-01-23 14:00:54 -0600

    Code example Two-Dimentional array

---
+++ C_NetCore2/Day02/Day02/Ex20_Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+using static System.Convert;
+
+
+namespace Day02
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //var charArray = new[] { 'a', 'e', 'i', 'o', 'u' };
+            //string[] cardinalDirections = { "North", "East", "South", "West" };
+            //char[] vowels = new char[5] { 'a', 'e', 'i', 'o', 'u' };
+            //WriteLine("One-Dimensional array.\n");
+            WriteLine("Array example.\n");
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            WriteLine("char[] vowels = {'a', 'e', 'i', 'o', 'u'};\n");
+            WriteLine("acces array using for loop");
+            for (int rankIndex = 0; rankIndex < vowels.Length; rankIndex++)
+            {
+                Write($"{vowels[rankIndex]} ");
+            }
+            WriteLine();
+            WriteLine("acces array using foreach loop");
+            foreach (char vowel in vowels)
+            {
+                Write($"{vowel} ");
+            }
+            ReadLine();
+        }
+       
+    }
+}
--- C_NetCore2/Day02/Day02/Program.cs
+++ C_NetCore2/Day02/Day02/Program.cs
@@ -12,23 +12,15 @@ namespace Day02
         
         static void Main(string[] args)
         {
-            //var charArray = new[] { 'a', 'e', 'i', 'o', 'u' };
-            //string[] cardinalDirections = { "North", "East", "South", "West" };
-            //char[] vowels = new char[5] { 'a', 'e', 'i', 'o', 'u' };
-            //WriteLine("One-Dimensional array.\n");
-            WriteLine("Array example.\n");
-            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
-            WriteLine("char[] vowels = {'a', 'e', 'i', 'o', 'u'};\n");
-            WriteLine("acces array using for loop");
-            for (int rankIndex = 0; rankIndex < vowels.Length; rankIndex++)
+            WriteLine("Two-Dimentional array. \n");
+
+            int[,] numbers = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+            for (int rowsIndex = 0; rowsIndex < 2; rowsIndex++)
             {
-                Write($"{vowels[rankIndex]} ");
-            }
-            WriteLine();
-            WriteLine("acces array using foreach loop");
-            foreach (char vowel in vowels)
-            {
-                Write($"{vowel} ");
+                for (int colIndex = 0; colIndex < 2; colIndex++)
+                {
+                    WriteLine($"numbers[{rowsIndex},{colIndex}] = {numbers[rowsIndex, colIndex]}");
+                }
             }
             ReadLine();
         }
