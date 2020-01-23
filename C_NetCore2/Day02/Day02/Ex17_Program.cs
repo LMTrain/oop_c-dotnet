a0754949cb09050820135309c85ee193cfa07c55
LM Train <layconmm@msn.com>
2020-01-23 12:40:38 -0600

    Code example on For continue statement in for Loop

---
+++ C_NetCore2/Day02/Day02/Ex17_Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+using static System.Convert;
+
+
+namespace Day02
+{
+    class Program
+    {
+        public abstract class Person
+        {
+            public abstract string Name { get; set; }
+        }
+
+        public class Author : Person
+        {
+            public override string Name { get; set; }
+        }
+
+        public class Reviewer : Person
+        {
+            public override string Name { get; set; }
+        }
+
+        public class TeamMember : Person
+        {
+            public override string Name { get; set; }
+            public void GetMemberName()
+            {
+                Console.WriteLine($"Member name:{Name}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            WriteLine("foreach loop example");
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            WriteLine("foreach on Array.");
+            foreach (var vowel in vowels)
+            {
+                WriteLine($"{vowel}");
+            }
+            WriteLine();
+            var persons = new List<Person>
+            {
+                new Author {Name = "Gaurav Aroraa"},
+                new Reviewer {Name = "Shivprasad Koirala"},
+                new TeamMember {Name = "Vikas Tiwari"},
+                new TeamMember {Name = "Denim Pinto"}
+            };
+            WriteLine("foreach on collection");
+            foreach (var person in persons)
+            {
+                WriteLine($"{person.Name}");
+            }
+        }
+       
+    }
+}
--- C_NetCore2/Day02/Day02/Program.cs
+++ C_NetCore2/Day02/Day02/Program.cs
@@ -9,51 +9,20 @@ namespace Day02
 {
     class Program
     {
-        public abstract class Person
-        {
-            public abstract string Name { get; set; }
-        }
-
-        public class Author : Person
-        {
-            public override string Name { get; set; }
-        }
-
-        public class Reviewer : Person
-        {
-            public override string Name { get; set; }
-        }
-
-        public class TeamMember : Person
-        {
-            public override string Name { get; set; }
-            public void GetMemberName()
-            {
-                Console.WriteLine($"Member name:{Name}");
-            }
-        }
+        
         static void Main(string[] args)
         {
-            WriteLine("foreach loop example");
-            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
-            WriteLine("foreach on Array.");
-            foreach (var vowel in vowels)
+            WriteLine("continue statement example");
+            WriteLine("continue in for loop");
+            for (int count = 0; count < 15; count++)
             {
-                WriteLine($"{vowel}");
+                if (count < 8)
+                {
+                    continue;
+                }
+                WriteLine($"{count}");
             }
-            WriteLine();
-            var persons = new List<Person>
-            {
-                new Author {Name = "Gaurav Aroraa"},
-                new Reviewer {Name = "Shivprasad Koirala"},
-                new TeamMember {Name = "Vikas Tiwari"},
-                new TeamMember {Name = "Denim Pinto"}
-            };
-            WriteLine("foreach on collection");
-            foreach (var person in persons)
-            {
-                WriteLine($"{person.Name}");
-            }
+            ReadLine();
         }
        
     }
