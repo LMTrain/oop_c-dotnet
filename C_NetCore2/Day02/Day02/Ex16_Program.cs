801521bf3b98b3bc7bb4d9110817b5038b228a9d
LM Train <layconmm@msn.com>
2020-01-23 12:34:26 -0600

    Code example on Foreach Loop

---
+++ C_NetCore2/Day02/Day02/Ex16_Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+
+namespace Day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("for loop example");
+            Write("Enter repeatitive length:");
+            int length = Convert.ToInt32(ReadLine());
+            for (int countIndex = 0; countIndex < length; countIndex++)
+            {               
+                WriteLine(new string('*', countIndex));
+            }           
+            ReadLine();
+        }
+       
+    }
+}
--- C_NetCore2/Day02/Day02/Program.cs
+++ C_NetCore2/Day02/Day02/Program.cs
@@ -2,22 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using static System.Console;
+using static System.Convert;
 
 
 namespace Day02
 {
     class Program
     {
+        public abstract class Person
+        {
+            public abstract string Name { get; set; }
+        }
+
+        public class Author : Person
+        {
+            public override string Name { get; set; }
+        }
+
+        public class Reviewer : Person
+        {
+            public override string Name { get; set; }
+        }
+
+        public class TeamMember : Person
+        {
+            public override string Name { get; set; }
+            public void GetMemberName()
+            {
+                Console.WriteLine($"Member name:{Name}");
+            }
+        }
         static void Main(string[] args)
         {
-            WriteLine("for loop example");
-            Write("Enter repeatitive length:");
-            int length = Convert.ToInt32(ReadLine());
-            for (int countIndex = 0; countIndex < length; countIndex++)
-            {               
-                WriteLine(new string('*', countIndex));
-            }           
-            ReadLine();
+            WriteLine("foreach loop example");
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            WriteLine("foreach on Array.");
+            foreach (var vowel in vowels)
+            {
+                WriteLine($"{vowel}");
+            }
+            WriteLine();
+            var persons = new List<Person>
+            {
+                new Author {Name = "Gaurav Aroraa"},
+                new Reviewer {Name = "Shivprasad Koirala"},
+                new TeamMember {Name = "Vikas Tiwari"},
+                new TeamMember {Name = "Denim Pinto"}
+            };
+            WriteLine("foreach on collection");
+            foreach (var person in persons)
+            {
+                WriteLine($"{person.Name}");
+            }
         }
        
     }
