8f625beafab82a2f178acf0710a88acce352d32c
LM Train <layconmm@msn.com>
2020-01-23 21:47:02 -0600

    Day03 - Code example on Infer Tuple

---
+++ C_NetCore2/Day03/Day03/Ex3_Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+using static System.Convert;
+
+namespace Day03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("C# 7.1 feature: default expression");
+            int thisIsANewDefault = default;
+            int thisIsAnOlderDefault = default;
+            var thisIsAnOlderDefaultAndValid = default(int);
+            //var thisIsNotValid = default; //Not valid, as we cannot assign default to implicit-typed variable
+            const int thisIsANewDefaultConst = default; //valid
+            const int thisIsAnOlderDefaultCont = default; //valid
+                                                          //const int? thisIsInvalid = default; //Invalid, as nullable cannot be declared const
+
+            WriteLine($"New default:{thisIsANewDefault}. Old default:{thisIsAnOlderDefault}");
+        }
+    }
+}
--- C_NetCore2/Day03/Day03/Program.cs
+++ C_NetCore2/Day03/Day03/Program.cs
@@ -6,20 +6,42 @@ using static System.Convert;
 
 namespace Day03
 {
+    public static void InferTupleNames(int num1, int num2)
+    {
+        (int, int) noNamed = (num1, num2);
+        Console.WriteLine($"NoNamed:{noNamed.Item1},{noNamed.Item2}");
+        (int, int) ignoredName = (A: num1, B: num2);
+        Console.WriteLine($"IgnoredName:{ignoredName.Item1},{ignoredName.Item2}");
+        (int a, int b) typeNamed = (num1, num2);
+        Console.WriteLine($"typeNamed using default member-names:{typeNamed.Item1},{typeNamed.Item2}");
+        Console.WriteLine($"typeNamed:{typeNamed.a},{typeNamed.b}");
+        var named = (num1, num2);
+        Console.WriteLine($"named using default member-names:{named.Item1},{named.Item2}");
+        Console.WriteLine($"named:{named.num1},{named.num2}");
+        var noNamedVariation = (num1, num1);
+        Console.WriteLine($"noNamedVariation:{noNamedVariation.Item1},{noNamedVariation.Item2}");
+        var explicitNaming = (n: num1, num1);
+        Console.WriteLine($"explicitNaming:{explicitNaming.n},{explicitNaming.num1}");
+        var partialnamed = (num1, 5);
+        Console.WriteLine($"partialnamed:{partialnamed.num1},{partialnamed.Item2}");
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            WriteLine("C# 7.1 feature: default expression");
-            int thisIsANewDefault = default;
-            int thisIsAnOlderDefault = default;
-            var thisIsAnOlderDefaultAndValid = default(int);
-            //var thisIsNotValid = default; //Not valid, as we cannot assign default to implicit-typed variable
-            const int thisIsANewDefaultConst = default; //valid
-            const int thisIsAnOlderDefaultCont = default; //valid
-                                                          //const int? thisIsInvalid = default; //Invalid, as nullable cannot be declared const
+            WriteLine("C# 7.1 feature: Infer tuple names");
+            Write("Enter first number:");
+            var number1 = ReadLine();
+            Write("Enter second number:");
+            var number2 = ReadLine();
+            InferTupleNames(Convert.ToInt32(number1), Convert.ToInt32(number2));
+            ReadLine();
+        }
 
-            WriteLine($"New default:{thisIsANewDefault}. Old default:{thisIsAnOlderDefault}");
+        private static void InferTupleNames(int v1, int v2)
+        {
+            throw new NotImplementedException();
         }
     }
 }
