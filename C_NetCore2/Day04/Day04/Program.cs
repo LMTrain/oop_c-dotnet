8944bf4ba3ae5d71a9e13424f3625aecfb2865ff
LM Train <layconmm@msn.com>
2020-01-23 22:25:40 -0600

    Day04 - Code example public class String Calculator

---
+++ C_NetCore2/Day04/Day04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace Day04
+{
+    class Program
+    {
+        internal class StringCalculator
+        {
+            public string Num1 { get; set; }
+            public string Num2 { get; set; }
+            public int Sum() => Convert.ToInt32(Num1) + Convert.ToInt32(Num2);
+        }
+        static void Main(string[] args)
+        {
+            Write("Enter number1:");
+            var num1 = ReadLine();
+            Write("Enter numbeer2:");
+            var num2 = ReadLine();
+            var sum = Convert.ToInt32(num1) +
+            Convert.ToInt32(num2);
+            Write($"Sum od {num1} and {num2} is {sum}");
+            ReadLine();
+        }
+    }
+}
