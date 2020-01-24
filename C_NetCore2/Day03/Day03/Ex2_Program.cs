6a1e6687e3f6883221c7b4e2a34323172b17d80e
LM Train <layconmm@msn.com>
2020-01-23 20:29:46 -0600

    Day03 - Code example on Default Expression

---
+++ C_NetCore2/Day03/Day03/Ex2_Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+using static System.Convert;
+
+namespace Day03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter first number: ");
+            var userInputFirst = ReadLine();
+            Write("Enter second number: ");
+            var userInputSecond = ReadLine();
+            var noNamed = OddEven.FindOddEvenBySingleNumber(Convert.ToInt32(userInputFirst));
+            var named = OddEven.FindOddEvenBySingleNumberNamedElement(Convert.ToInt32(userInputSecond));
+            WriteLine($"First Number:{noNamed.Item1} is {noNamed.Item2} using noNamed tuple.");
+            WriteLine($"Second Number:{named.number} is {named.oddOrEven} using Named tuple.");
+
+            WriteLine("Assigning 'Named' to 'NoNamed'");
+            noNamed = named;
+            WriteLine($"Number:{noNamed.Item1} is {named.Item2} after assignment.");
+            Write("Enter third number: ");
+            var userInputThird = ReadLine();
+            var noNamed2 = OddEven.FindOddEvenBySingleNumber(Convert.ToInt32(userInputThird));
+            WriteLine(
+                $"Third Number:{noNamed2.Item1} is {noNamed2.Item2} using second noNamed tuple.");
+            WriteLine("Assigning 'second NoNamed' to 'Named'");
+            named = noNamed2;
+            WriteLine($"Second Number:{named.number} is {named.oddOrEven} after assignment.");
+            ReadLine();
+        }
+    }
+}
--- C_NetCore2/Day03/Day03/Program.cs
+++ C_NetCore2/Day03/Day03/Program.cs
@@ -10,27 +10,16 @@ namespace Day03
     {
         static void Main(string[] args)
         {
-            Write("Enter first number: ");
-            var userInputFirst = ReadLine();
-            Write("Enter second number: ");
-            var userInputSecond = ReadLine();
-            var noNamed = OddEven.FindOddEvenBySingleNumber(Convert.ToInt32(userInputFirst));
-            var named = OddEven.FindOddEvenBySingleNumberNamedElement(Convert.ToInt32(userInputSecond));
-            WriteLine($"First Number:{noNamed.Item1} is {noNamed.Item2} using noNamed tuple.");
-            WriteLine($"Second Number:{named.number} is {named.oddOrEven} using Named tuple.");
+            WriteLine("C# 7.1 feature: default expression");
+            int thisIsANewDefault = default;
+            int thisIsAnOlderDefault = default;
+            var thisIsAnOlderDefaultAndValid = default(int);
+            //var thisIsNotValid = default; //Not valid, as we cannot assign default to implicit-typed variable
+            const int thisIsANewDefaultConst = default; //valid
+            const int thisIsAnOlderDefaultCont = default; //valid
+                                                          //const int? thisIsInvalid = default; //Invalid, as nullable cannot be declared const
 
-            WriteLine("Assigning 'Named' to 'NoNamed'");
-            noNamed = named;
-            WriteLine($"Number:{noNamed.Item1} is {named.Item2} after assignment.");
-            Write("Enter third number: ");
-            var userInputThird = ReadLine();
-            var noNamed2 = OddEven.FindOddEvenBySingleNumber(Convert.ToInt32(userInputThird));
-            WriteLine(
-                $"Third Number:{noNamed2.Item1} is {noNamed2.Item2} using second noNamed tuple.");
-            WriteLine("Assigning 'second NoNamed' to 'Named'");
-            named = noNamed2;
-            WriteLine($"Second Number:{named.number} is {named.oddOrEven} after assignment.");
-            ReadLine();
+            WriteLine($"New default:{thisIsANewDefault}. Old default:{thisIsAnOlderDefault}");
         }
     }
 }
